b1c9b5cd06f3ae07dab98d5bc1465d92fe5a8bef
douglas ogisi <douglas.ogisi@gmail.com>
2017-11-04 11:03:34 +0000

    Centralized API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1, "firstname":"Bob","lastname":"Smith","email":"bob@gmail.com"},
+    {"id": 2, "firstname":"Tammy","lastname":"Norton","email":"tnorton@gmail.com"},
+    {"id": 3, "firstname":"Tina","lastname":"lee","email":"lee.tina@gmail.com"}
+  ])
+});
+
 app.listen(port, function(err){
   if (err) {
     console.log(err);
